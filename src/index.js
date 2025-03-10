1a3ac90bdf6e98765e996f777cc8da34666e5983
BizzNist <bizznist@gmail.com>
2025-03-10 21:24:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,8 +9,11 @@ import {
 
 import './style.css'
 import Home from './views/home'
-import About from './views/about'
-import Contact from './views/contact'
+import About from './views/about-us'
+import Contact from './views/contact-us'
+import Cookies from './views/cookies-settings'
+import Privacy from './views/privacy-policy'
+import Terms from './views/terms-of-service'
 import NotFound from './views/not-found'
 
 const App = () => {
@@ -18,8 +21,11 @@ const App = () => {
     <Router>
       <Switch>
         <Route component={Home} exact path="/" />
-        <Route component={About} exact path="/about" />
-        <Route component={Contact} exact path="/contact" />
+        <Route component={About} exact path="/about-us" />
+        <Route component={Contact} exact path="/contact-us" />
+        <Route component={cookies} exact path="/cookies-settings" />
+        <Route component={privacy} exact path="/privacy-policy" />
+        <Route component={terms} exact path="/terms-of-service" />
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
